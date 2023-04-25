1fdb7e65282f1af4fde24eaad3454816a26898d6
Orinami Kure <namykure@gmail.com>
2023-04-25 22:18:05 +0100

    chore: Modify add computer scenario to include confirming that new computer is in list

--- cypress/e2e/AddComputer-ValidScenario.cy.js
+++ cypress/e2e/AddComputer-ValidScenario.cy.js
@@ -1,4 +1,4 @@
-import{addComputer} from "../support/Comp-Selector.cy";
+import{addComputer, filterList} from "../support/Comp-Selector.cy";
 
 describe('Given that I am on the add computer page', () => {
 
@@ -7,19 +7,22 @@ describe('Given that I am on the add computer page', () => {
     cy.wait(3000)
     cy.get(addComputer.computerNameInput).clear().type(addComputer.computerNameText3)
     cy.get(addComputer.introducedInput).clear().type(addComputer.introducedText3)
-    
     cy.get(addComputer.companyDropdown).select(7)
     cy.get(addComputer.createComputerBtn).click({force:true})
   })
 
-  it ('Valid Inputs - I should be able to add a computer', () => {
+  it ('Valid Inputs - I should be able to add a computer and view recent addition', () => {
     cy.visit('/new')
     cy.wait(3000)
     cy.get(addComputer.computerNameInput).clear().type(addComputer.computerNameText2)
     cy.get(addComputer.introducedInput).clear().type(addComputer.introducedText2)
     cy.get(addComputer.discontinuedInput).clear().type(addComputer.discontinuedText2)
     cy.get(addComputer.companyDropdown).select(9)
-    cy.get(addComputer.createComputerBtn).click({force:true})
+    cy.get(addComputer.createComputerBtn).click({force:true}).wait(4000)
+    //Locate the tabular list of computers
+    cy.get('.computers').as('computersTable');
+    // Assert that the newly created computer exists in the tabular list
+    cy.get('@computersTable').contains('td', 'Axel').should('exist');
   })
   
 })
